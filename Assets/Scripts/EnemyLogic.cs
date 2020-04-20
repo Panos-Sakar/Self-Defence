0e384d65b9d46bcb98d97338b626bb2a6a4a1332
DESKTOPPtol\Sakar <panosxsakar@gmail.com>
2020-04-20 17:10:38 +0300

    Added player and enemy prefubs. Lighting init

---
+++ Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyLogic : MonoBehaviour
+{
+    private GameObject _player;
+    private NavMeshAgent _enemyNavMesh;
+    private Transform _playerTransform;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _enemyNavMesh = GetComponent<NavMeshAgent>();
+        _playerTransform = _player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _enemyNavMesh.destination = _playerTransform.position;
+    }
+}
